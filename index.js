1033fab82e75e4f0a01205366365814886f18682
lekan <laksidesvibe@gmail.com>
2023-04-05 01:26:43 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+
+
+function superbowlWin(record) {   
+    const win = record.find(record => record.result === "W"); // Use find() to search for an object with result === "W" 
+    return win ? win.year : undefined; // If win is found, return the year property. Otherwise, return undefined.
+}
